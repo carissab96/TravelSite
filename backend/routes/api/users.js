8db59ff01e7ef6de02d3f7a237b50ac555e9d943
carissab96 <bayscarissa706@gmail.com>
2024-12-15 09:59:30 -0600

    add user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,27 +5,65 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
 
 // Sign up
 router.post(
-    '/',
-    async (req, res) => {
-      const { email, password, username } = req.body;
+  '/',
+  validateSignup,
+  async (req, res, next) => {
+    const { email, password, username } = req.body;
+
+    try {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({ email, username, hashedPassword });
-  
+
       const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
       };
-  
+
       await setTokenCookie(res, safeUser);
-  
+
       return res.json({
         user: safeUser
       });
+    } catch (e) {
+      if (e.name === 'SequelizeUniqueConstraintError') {
+        const errors = {};
+        if (e.fields.email) errors.email = 'User with that email already exists';
+        if (e.fields.username) errors.username = 'User with that username already exists';
+        
+        const err = Error("User already exists");
+        err.errors = errors;
+        err.status = 403;
+        err.title = "User already exists";
+        return next(err);
+      }
+      return next(e);
     }
-  );
-  
+  }
+);  
 module.exports = router;
