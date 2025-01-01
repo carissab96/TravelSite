efed28c4f136917635fda0d72d88272e35ec65b1
carissab96 <bayscarissa706@gmail.com>
2025-01-01 15:48:27 -0600

    another quick fix??

--- backend/db/seeders/20231215160335-demo-user.js
+++ backend/db/seeders/20231215160335-demo-user.js
@@ -6,7 +6,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-
+const { User } = require('../models'); // Import the User model
 module.exports = {
   async up (queryInterface, Sequelize) {
   //   options.tableName = 'Users';
