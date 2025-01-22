f08802378ea04a4e95be78eb0ad828f9fac73dc9
carissab96 <bayscarissa706@gmail.com>
2025-01-22 08:57:20 -0600

    modified api/session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -36,8 +36,8 @@ router.post(
       }
     });
 
-    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-      const err = new Error('Login failed');
+    if (!user || !bcrypt.compareSync(password, user?.hashedPassword?.toString())) {
+      const err = new Error('Invalid credentials');
       err.status = 401;
       err.title = 'Login failed';
       err.errors = { credential: 'The provided credentials were invalid.' };
